7f3acb9fb205153fde0f7b376ff35db3a7673c3a
Nidal Mohammed <nidalmo23@gmail.com>
2023-04-01 08:33:32 +0530

    auth route updated

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRoute from './routes/user.route.js'
+import authRoute from './routes/auth.route.js'
 import gigRoute from "./routes/gig.route.js";
 import orderRoute from "./routes/order.route.js";
 import conversationRoute from "./routes/conversation.route.js";
@@ -11,7 +12,7 @@ import reviewRoute from "./routes/review.route.js";
 const app = express();
 dotenv.config()
 
-
+app.use("/api/auth", authRoute);
 app.use('/api/user', userRoute)
 app.use("/api/gigs", gigRoute);
 app.use("/api/orders", orderRoute);
