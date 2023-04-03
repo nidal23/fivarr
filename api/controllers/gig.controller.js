68289b6d5417140ec5ae6d28f058a59ecc77f91a
Nidal Mohammed <nidalmo23@gmail.com>
2023-04-03 06:18:37 +0530

    updated gig backend

--- api/controllers/gig.controller.js
+++ api/controllers/gig.controller.js
@@ -1,5 +1,64 @@
-export const fn = (req, res) => {
-    res.send('delete user')
+import Gig from "../models/gig.model"
+import createError from "../utils/createError";
+
+export const createGig = async (req, res, next) => {
+    if (!req.isSeller) return next(createError(403, "Sign in with a seller account to create a gig"))
+
+    const newGig = new Gig({
+        userId: req.userId,
+        ...req.body
+    })
+
+    try {
+        const savedGig = await newGig.save();
+        res.status(201).json(savedGig)
+
+    } catch (err) {
+        next(err)
+    }
+}
+
+export const deleteGig = async (req, res, next) => {
+    try {
+        const gig = await Gig.findById(req.params.id)
+
+        if (gig.userId !== req.userId) return next(createError(403, "You're not the owner"))
+
+        await Gig.findByIdAndDelete(req.params.id)
+        res.status(200).send("Gig has been deleted")
+    } catch (err) {
+        next(err)
+    }
+}
+
+export const getGig = async (req, res, next) => {
+    try {
+        const gig = await Gig.findById(req.params.id)
+        if (!gig) return next(createError(404, 'Gig not found'))
+        res.status(200).json(gig)
+    } catch (err) {
+        next(err)
+    }
+}
+
+
+export const getGigs = async (req, res, next) => {
+    const q = req.query;
+    const filters = {
+        ...(q.userId && { userId: q.userId }),
+        ...((q.min || q.max) && {
+            price: { ...(q.min && { $gt: q.min }), ...(q.max && { $lt: q.max }) },
+        }),
+        ...(q.search && { title: { $regex: q.search, $options: "i" } }),
+
+
+    }
+    try {
+        const gigs = await Gig.find(filters)
+        res.status(200).json(gigs)
+    } catch (err) {
+        next(err)
+    }
 }
 
 
--- api/routes/gig.route.js
+++ api/routes/gig.route.js
@@ -1,10 +1,19 @@
-import express from 'express'
-import { fn } from '../controllers/gig.controller.js'
+import express from "express";
+import {
+    createGig,
+    deleteGig,
+    getGig,
+    getGigs
+} from "../controllers/gig.controller.js";
+import { verifyToken } from "../middleware/jwt.js";
 
 const router = express.Router()
 
 
 
-router.get('/', fn)
+router.post('/', verifyToken, createGig)
+router.delete('/:id', verifyToken, deleteGig)
+router.get('/single/:id', verifyToken, getGig)
+router.get('/', verifyToken, getGigs)
 
 export default router;
